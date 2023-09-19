ba194ebb071817fae6535bea7d66c46ccd233ced
bangchisi <ahdtpf13@naver.com>
2023-09-19 15:44:04 +0900

    add state in Annotations for passing props to Category and AnnotationList

--- src/routes/Annotator/components/RightSidebar/Controls/Annotations/Annotations.tsx
+++ src/routes/Annotator/components/RightSidebar/Controls/Annotations/Annotations.tsx
@@ -1,12 +1,22 @@
 import Categories from './Categories/Categories';
 import AnnotationList from './AnnotationList/AnnotationList';
 import { Container } from './Annotatoins.style';
+import { ChangeEvent, useState } from 'react';
 
 export default function Annotations() {
+  const [currentCategory, setCurrentCategory] = useState<string>('thing');
+
+  const handleCategoryChange = (event: ChangeEvent<HTMLSelectElement>) => {
+    setCurrentCategory(event.target.value);
+  };
+
   return (
     <Container>
-      <Categories />
-      <AnnotationList />
+      <Categories
+        currentCategory={currentCategory}
+        handleChange={handleCategoryChange}
+      />
+      <AnnotationList currentCategory={currentCategory} />
     </Container>
   );
 }
