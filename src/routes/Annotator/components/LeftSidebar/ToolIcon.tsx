f8f8e0d64c8cc5ba945bcd7be36c08793aba58d4
zsef123 <zsef123@gmail.com>
2023-09-19 13:36:21 +0000

    [WIP] Add polygon tools

--- src/routes/Annotator/components/LeftSidebar/ToolIcon.tsx
+++ src/routes/Annotator/components/LeftSidebar/ToolIcon.tsx
@@ -4,11 +4,14 @@ import { ReactNode } from 'react';
 export default function ToolIcon(props: {
   toolName: string;
   iconComponent: ReactNode;
+  onClick?: () => void;
 }) {
   // Box, brush, eraser, sam
   return (
     <Tooltip title={props.toolName} placement="right">
-      <IconButton key={props.toolName}>{props.iconComponent}</IconButton>
+      <IconButton key={props.toolName} onClick={props.onClick}>
+        {props.iconComponent}
+      </IconButton>
     </Tooltip>
   );
 }
--- src/routes/Annotator/components/Workbench/Canvas/Canvas.tsx
+++ src/routes/Annotator/components/Workbench/Canvas/Canvas.tsx
@@ -6,18 +6,111 @@ import {
   onCanvasWheel,
 } from './helpers/canvasHelper';
 import { Editor } from './Canvas.style';
+import { Tool } from 'routes/Annotator/Annotator';
+import { PaperClassKey } from '@mui/material';
 
 interface CanvasProps {
+  selectedTool: Tool;
   containerWidth: number | null;
   containerHeight: number | null;
 }
 
+const SelectTool = (
+  initPoint: any,
+  containerWidth: any,
+  containerHeight: any,
+  imgWidth: any,
+  imgHeight: any,
+) => {
+  paper.view.onMouseDown = (event: paper.MouseEvent) => {
+    initPoint = event.point;
+    if (containerWidth && containerHeight && imgWidth && imgHeight) {
+      console.log('trying drawAnnotation()');
+      drawAnnotation(containerWidth, containerHeight, imgWidth, imgHeight);
+      console.log(
+        event.point.subtract(
+          new paper.Point(containerWidth / 2, containerHeight / 2),
+        ),
+      );
+    }
+  };
+
+  paper.view.onMouseMove = (event: paper.MouseEvent) => {
+    initPoint = event.point;
+  };
+
+  paper.view.onMouseDrag = (event: paper.MouseEvent) => {
+    console.log('onMouseDrag', event);
+    if (initPoint) {
+      const delta_x: number | null = initPoint.x - event.point.x;
+      const delta_y: number | null = initPoint.y - event.point.y;
+      const center_delta = new paper.Point(delta_x, delta_y);
+      const new_center = paper.view.center.add(center_delta);
+      paper.view.center = new_center;
+      console.log(paper.view.center);
+    }
+  };
+};
+
+const PolygonTool = () => {
+  let myPath: any = null;
+  const strokeColor = new paper.Color('black');
+
+  let brush_path: any = null;
+  const createBrush = (center? : any) => {
+    center = center || new paper.Point(0, 0);
+    brush_path = new paper.Path.Circle({
+      center: center,
+      strokeColor: strokeColor,
+      strokeWidth: 30,
+      radius: 10,
+    });
+  };
+
+  const createSelection = () => {
+    // do nothing
+  };
+  
+
+  paper.view.onMouseDown = (event: paper.ToolEvent) => {
+    console.log('Polygon Mouse Down', event);
+    myPath = new paper.Path();
+    myPath.strokeColor = new paper.Color('black');
+  };
+
+  paper.view.onMouseDrag = (event: paper.ToolEvent) => {
+    console.log('Polygon Mouse Drag', event);
+    const circle = new paper.Path.Circle({
+      center: event.middlePoint,
+      radius: event.delta.length / 2,
+    });
+    circle.fillColor = new paper.Color('white');
+  };
+
+  paper.view.onMouseUp = (event: paper.ToolEvent) => {
+    console.log('Polygon Mouse Up', event);
+    const myCircle = new paper.Path.Circle({
+      center: event.point,
+      radius: 10,
+    });
+    myCircle.strokeColor = new paper.Color('black');
+    myCircle.fillColor = new paper.Color('white');
+  };
+};
+
 // TODO: paper init to another file?
 export default function Canvas({
+  selectedTool,
   containerWidth,
   containerHeight,
 }: CanvasProps) {
-  let initPoint: paper.Point | null = null;
+  // if (selectedTool == Tool.Select) {
+  //   console.log('Go Select');
+  // } else if (selectedTool == Tool.Polygon) {
+  //   PolygonTool();
+  // }
+
+  const initPoint: paper.Point | null = null;
   let imgWidth: number | null = null;
   let imgHeight: number | null = null;
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
@@ -61,33 +154,18 @@ export default function Canvas({
         }
       });
 
-      paper.view.onMouseDown = (event: paper.MouseEvent) => {
-        initPoint = event.point;
-        if (containerWidth && containerHeight && imgWidth && imgHeight) {
-          console.log('trying drawAnnotation()');
-          drawAnnotation(containerWidth, containerHeight, imgWidth, imgHeight);
-          console.log(
-            event.point.subtract(
-              new paper.Point(containerWidth / 2, containerHeight / 2),
-            ),
-          );
-        }
-      };
-
-      paper.view.onMouseMove = (event: paper.MouseEvent) => {
-        initPoint = event.point;
-      };
-
-      paper.view.onMouseDrag = (event: paper.MouseEvent) => {
-        if (initPoint) {
-          const delta_x: number | null = initPoint.x - event.point.x;
-          const delta_y: number | null = initPoint.y - event.point.y;
-          const center_delta = new paper.Point(delta_x, delta_y);
-          const new_center = paper.view.center.add(center_delta);
-          paper.view.center = new_center;
-          console.log(paper.view.center);
-        }
-      };
+      if (selectedTool == Tool.Select) {
+        SelectTool(
+          initPoint,
+          containerWidth,
+          containerHeight,
+          imgWidth,
+          imgHeight,
+        );
+      } else if (selectedTool == Tool.Polygon) {
+        console.log('Go Polygon');
+        PolygonTool();
+      }
 
       canvas.onwheel = onCanvasWheel;
 
@@ -98,7 +176,7 @@ export default function Canvas({
         paper.view.onMouseDrag = null;
       };
     }
-  }, []);
+  }, [selectedTool]);
 
   useEffect(() => {
     if (containerWidth && containerHeight) {
