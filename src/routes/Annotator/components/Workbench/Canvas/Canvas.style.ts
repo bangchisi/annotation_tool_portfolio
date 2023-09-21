20442de8834a1add0c72a120a11ea15fff402e31
bangchisi <ahdtpf13@naver.com>
2023-09-21 21:17:11 +0900

    add enum Tool in Canvas.style.ts for bug fix

--- src/routes/Annotator/components/Workbench/Canvas/Canvas.style.ts
+++ src/routes/Annotator/components/Workbench/Canvas/Canvas.style.ts
@@ -1,24 +1,32 @@
 import { styled } from '@mui/material';
-import { Tool } from 'routes/Annotator/Annotator';
 
 interface EditorProps {
   selectedTool: Tool;
 }
 
-// const CursorTypes = {
-//   [Tool.Select]: 'pointer',
-//   [Tool.Box]: 'crosshair',
-//   [Tool.Brush]: 'default',
-//   [Tool.Eraser]: 'default',
-//   [Tool.SAM]: 'crosshair',
-// };
+enum Tool {
+  Select,
+  Box,
+  Brush,
+  Eraser,
+  SAM,
+}
 
-// export const Editor = styled('canvas')<EditorProps>(({ selectedTool }) => {
-export const Editor = styled('canvas')<EditorProps>(() => {
+const CursorTypes = {
+  [Tool.Select]: 'pointer',
+  [Tool.Box]: 'crosshair',
+  [Tool.Brush]: 'none',
+  [Tool.Eraser]: 'default',
+  [Tool.SAM]: 'crosshair',
+};
+
+// export const Editor = styled('canvas')<EditorProps>(() => {
+export const Editor = styled('canvas')<EditorProps>(({ selectedTool }) => {
   return {
     width: '100%',
     height: 'auto',
-    backgroundColor: 'white',
-    // cursor: CursorTypes[selectedTool] ?? 'default',
+    backgroundColor: 'rgba(215, 215, 215, 1.0)',
+    // backgroundColor: 'white',
+    cursor: CursorTypes[selectedTool] ?? 'default',
   };
 });
