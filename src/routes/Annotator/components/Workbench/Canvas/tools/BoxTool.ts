a8f7f9053800ec98a96d1ec77b5a97316cac4a96
bangchisi <ahdtpf13@naver.com>
2023-09-21 21:00:50 +0900

    box tool file separation

--- src/routes/Annotator/components/Workbench/Canvas/tools/BoxTool.ts
+++ src/routes/Annotator/components/Workbench/Canvas/tools/BoxTool.ts
@@ -1,60 +1,56 @@
 import paper from 'paper';
 
-/** Box 그리는 흐름
- * 1. 첫 클릭 = start 정함
- * 2. 두번째 클릭 = end 정함
- * 3. start, end로 Box 생성
- */
+let currentBox: paper.Path.Rectangle | null;
+let startPoint: paper.Point | null;
+let endPoint: paper.Point | null;
 
-// 변수 초기화
-let isDrawing = false; // 그리기 모드 여부
-let startPoint: paper.Point | null = null; // 첫 번째 지점
-let currentBox: paper.Path.Rectangle | null = null; // 현재 그리고 있는 박스
-
-// 마우스 다운 이벤트 핸들러
-export const onBoxMouseDown = (event: paper.MouseEvent) => {
-  console.log('box start');
-  if (!isDrawing) {
-    // 그리기 모드 시작
-    startPoint = event.point;
-    isDrawing = true;
-  }
-};
-
-// 마우스 무브 이벤트 핸들러
-console.log('box move');
 export const onBoxMouseMove = (event: paper.MouseEvent) => {
-  if (isDrawing) {
+  if (!startPoint) return;
+
+  if (startPoint) {
     if (currentBox) {
-      // 이미 그리고 있는 박스가 있으면 제거
+      // remove current box
       currentBox.remove();
+      currentBox = null;
     }
 
-    // 현재 위치까지 박스 그리기
+    // create current box
     currentBox = new paper.Path.Rectangle({
       from: startPoint,
       to: event.point,
-      strokeColor: 'black',
+      strokeWidth: 2,
+      strokeColor: new paper.Color(1, 1, 1, 1),
     });
   }
 };
 
-// // 마우스 업 이벤트 핸들러
-export const onBoxMouseUp = (event: paper.MouseEvent) => {
-  console.log('box end');
-  if (isDrawing) {
-    // 그리기 모드 종료
-    isDrawing = false;
+export const onBoxMouseDown = (event: paper.MouseEvent) => {
+  if (!startPoint) {
+    // set start point
+    startPoint = event.point;
+  } else {
+    // set end point
+    endPoint = event.point;
+  }
+};
 
-    // 최종 박스 그리기
+export const onBoxMouseUp = (event: paper.MouseEvent) => {
+  if (startPoint && endPoint) {
+    // draw box
+    new paper.Path.Rectangle({
+      from: startPoint,
+      to: event.point,
+      strokeWidth: 2,
+      strokeColor: new paper.Color(1, 0, 0, 1),
+    });
+    startPoint = null;
+    endPoint = null;
+
     if (currentBox) {
+      // remove current box
       currentBox.remove();
-      new paper.Path.Rectangle({
-        from: startPoint,
-        to: event.point,
-        strokeColor: 'black',
-      });
-      startPoint = null; // 첫 번째 지점 초기화
+      currentBox = null;
     }
+    console.dir(paper.project.activeLayer.children);
   }
 };
