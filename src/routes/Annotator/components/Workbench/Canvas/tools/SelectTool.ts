e932cdd3f6127912e2637afa29e74fae50ea3263
bangchisi <ahdtpf13@naver.com>
2023-09-21 21:00:37 +0900

    select tool file separation

--- src/routes/Annotator/components/Workbench/Canvas/tools/SelectTool.ts
+++ src/routes/Annotator/components/Workbench/Canvas/tools/SelectTool.ts
@@ -1,37 +1,8 @@
 import paper from 'paper';
 
-export const onSelectMouseMove = (
-  event: paper.MouseEvent,
-  onChangePoint: (point: paper.Point) => void,
-  // containerWidth: number | null,
-  // containerHeight: number | null,
-) => {
-  const { point } = event;
-  // let tempPoint: paper.Point | null = null;
-
-  // if (containerWidth && containerHeight) {
-  //   const containerPoint = new paper.Point(
-  //     containerWidth / 2,
-  //     containerHeight / 2,
-  //   );
-
-  //   // 이미지 가운데가 (0, 0)인 좌표로 변환
-  //   tempPoint = point.subtract(containerPoint);
-  //   onChangePoint(tempPoint);
-  //   // console.log('SelectTool.ts, onSelectMouseMove, tempPoint: ', tempPoint);
-  // } else {
-  //   // 그냥 그대로, 혹시 모르니
-  //   onChangePoint(point);
-  // }
-  onChangePoint(point);
-};
-
 // 마우스 버튼 누름
 export const onSelectMouseDown = (event: paper.MouseEvent) => {
-  console.log(
-    'SelectTool.ts, onSelectMouseDown, actual point is ',
-    event.point,
-  );
+  console.log('SelectTool.ts, onSelectMouseDown', event);
 };
 
 // 마우스 드래그
@@ -39,7 +10,6 @@ export const onSelectMouseDrag = (
   event: paper.MouseEvent,
   initPoint: paper.Point | null,
 ) => {
-  console.log('onMouseDrag', event);
   // view.center를 마우스 delta만큼 옮김
   if (initPoint) {
     const delta_x: number | null = initPoint.x - event.point.x;
@@ -47,6 +17,5 @@ export const onSelectMouseDrag = (
     const center_delta = new paper.Point(delta_x, delta_y);
     const new_center = paper.view.center.add(center_delta);
     paper.view.center = new_center;
-    // console.log(paper.view.center);
   }
 };
