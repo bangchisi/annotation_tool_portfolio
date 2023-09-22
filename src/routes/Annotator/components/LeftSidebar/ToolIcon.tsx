1290f30cd26e3b025b687aa439db79719b30ff39
bangchisi <ahdtpf13@naver.com>
2023-09-22 14:19:32 +0900

    ToolIcon tool tip position changed

--- src/routes/Annotator/components/LeftSidebar/ToolIcon.tsx
+++ src/routes/Annotator/components/LeftSidebar/ToolIcon.tsx
@@ -5,10 +5,24 @@ export default function ToolIcon(props: {
   toolName: string;
   iconComponent: ReactNode;
   onClick?: () => void;
+  placement?:
+    | 'bottom-end'
+    | 'bottom-start'
+    | 'bottom'
+    | 'left-end'
+    | 'left-start'
+    | 'left'
+    | 'right-end'
+    | 'right-start'
+    | 'right'
+    | 'top-end'
+    | 'top-start'
+    | 'top';
 }) {
   // Box, brush, eraser, sam
+  const { placement } = props;
   return (
-    <Tooltip title={props.toolName} placement="right">
+    <Tooltip title={props.toolName} placement={placement || 'right'}>
       <IconButton key={props.toolName} onClick={props.onClick}>
         {props.iconComponent}
       </IconButton>
