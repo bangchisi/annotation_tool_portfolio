f071504042b498bd1fc2c8c09c03b5993d37c080
bangchisi <ahdtpf13@naver.com>
2023-09-25 13:14:22 +0900

    add annotator axios request files

--- src/routes/Annotator/models/Annotator.model.ts
+++ src/routes/Annotator/models/Annotator.model.ts
@@ -1,1 +1,11 @@
 import axios from 'axios';
+
+const baseURL = '/api/annotator/';
+
+const AnnotatorModel = {
+  getCategories: (datasetId: number, imageId: number) => {
+    return axios.get(`${baseURL}/${datasetId}/${imageId}`);
+  },
+};
+
+export default AnnotatorModel;
