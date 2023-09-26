44ae2c293bad0e6516a5f39c790c9dd847629e53
bangchisi <ahdtpf13@naver.com>
2023-09-26 16:38:36 +0900

    get categories api

--- src/routes/Annotator/models/Annotator.model.ts
+++ src/routes/Annotator/models/Annotator.model.ts
@@ -2,9 +2,51 @@ import axios from 'axios';
 
 const baseURL = '/api/annotator/';
 
+const initialCategories = {
+  data: [
+    {
+      id: 1,
+      name: 'human',
+      annotations: [],
+    },
+    {
+      id: 2,
+      name: 'animal',
+      annotations: [],
+    },
+    {
+      id: 3,
+      name: 'building',
+      annotations: [],
+    },
+    {
+      id: 4,
+      name: 'machine',
+      annotations: [],
+    },
+  ],
+};
+
 const AnnotatorModel = {
-  getCategories: (datasetId: number, imageId: number) => {
-    return axios.get(`${baseURL}/${datasetId}/${imageId}`);
+  getCategories: async (datasetId: number, imageId: number) => {
+    try {
+      const response =
+        process.env.NODE_ENV === 'development'
+          ? initialCategories
+          : await axios.get(`${baseURL}/${datasetId}/${imageId}`);
+
+      console.log('Annotator.model.ts, getCategories()');
+      console.log(response.data);
+
+      return response.data;
+    } catch (error) {
+      console.log('Failed to get categories');
+      if (error instanceof Error) {
+        console.log(error.stack);
+      }
+
+      return null;
+    }
   },
 };
 
