6fb302f3f5392a746e28f5f633c9a1a775063285
bangchisi <ahdtpf13@naver.com>
2023-09-27 14:41:19 +0900

    set current annotation on click annotation in annotation list

--- src/routes/Annotator/components/RightSidebar/Controls/Annotations/AnnotationList/Annotation/Annotation.tsx
+++ src/routes/Annotator/components/RightSidebar/Controls/Annotations/AnnotationList/Annotation/Annotation.tsx
@@ -1,5 +1,31 @@
+import { AnnotationType } from 'routes/Annotator/Annotator.types';
 import { Container } from './Annotation.style';
+import { useAppDispatch } from 'App.hooks';
+import { setCurrentAnnotation } from 'routes/Annotator/slices/annotatorSlice';
 
-export function Annotation() {
-  return <Container>아이디</Container>;
+interface AnnotationProps {
+  annotation: AnnotationType;
+}
+
+export function Annotation({ annotation }: AnnotationProps) {
+  const dispatch = useAppDispatch();
+
+  const handleClick = () => {
+    console.log('select annotation');
+    if (annotation) {
+      console.dir(annotation);
+
+      dispatch(setCurrentAnnotation({ currentAnnotation: annotation }));
+    }
+  };
+
+  return (
+    <Container onClick={handleClick}>
+      {annotation && (
+        <div>
+          <span>(id: {annotation.id})</span>
+        </div>
+      )}
+    </Container>
+  );
 }
