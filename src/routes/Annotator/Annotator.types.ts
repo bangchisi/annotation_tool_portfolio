2d8b932bab04152ae2efc38fe68f37cf7475e513
bangchisi <ahdtpf13@naver.com>
2023-10-03 19:22:46 +0900

    changed annotationtype

--- src/routes/Annotator/Annotator.types.ts
+++ src/routes/Annotator/Annotator.types.ts
@@ -1,7 +1,13 @@
-export interface AnnotationType {
-  annotationId?: number;
-  category?: string;
-  path?: paper.CompoundPath | paper.PathItem | null;
-  // points: number[][];
-  // color: string;
+export interface CategoryType {
+  id: number;
+  name: string;
+  annotations: AnnotationType[];
 }
+
+export interface AnnotationType {
+  id?: number;
+  categoryId?: number;
+  path?: paper.CompoundPath | paper.PathItem | null;
+}
+
+export type CategoriesType = CategoryType[];
