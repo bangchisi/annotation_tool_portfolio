a9f0ff5700143a18aaa5a1256a05a48b7bc94c0c
bangchisi <ahdtpf13@naver.com>
2023-10-04 14:33:54 +0900

    brush tool path changed. pathItem -> compoundPath

--- src/routes/Annotator/components/Workbench/Canvas/tools/BrushTool.ts
+++ src/routes/Annotator/components/Workbench/Canvas/tools/BrushTool.ts
@@ -3,7 +3,7 @@ import { updateAnnotation } from 'routes/Annotator/slices/annotatorSlice';
 import { AppDispatch } from 'store';
 
 // radius will change when preferences panel is implemented.
-let selection: paper.PathItem | null = null;
+let selection: paper.CompoundPath | null = null;
 let brushCursor: paper.Path.Circle | null = null;
 const radius = 20;
 const fillColor = new paper.Color(1, 1, 1, 0.2);
@@ -27,13 +27,18 @@ export const onBrushMouseDown = (event: paper.MouseEvent) => {
   console.group('%cbrush down', 'color: red');
   // selection이 없을때만 selection을 현재 위치 원으로 생성
   if (!selection) {
-    selection = new paper.Path.Circle({
-      center: event.point,
-      radius,
-      fillColor,
-      strokeColor,
-      strokeWidth,
-    });
+    selection = new paper.CompoundPath(
+      new paper.Path.Circle({
+        center: event.point,
+        radius,
+        fillColor,
+        strokeColor,
+        strokeWidth,
+      }),
+    );
+    selection.fillColor = fillColor;
+    selection.strokeColor = strokeColor;
+    selection.strokeWidth = strokeWidth;
   }
   console.groupEnd();
 };
@@ -60,7 +65,7 @@ export const onBrushMouseUp = (dispatch: AppDispatch) => {
   dispatch(updateAnnotation({ path: JSON.parse(JSON.stringify(selection)) }));
   selection?.remove();
   selection = null;
-  // console.log(paper.project.activeLayer.children);
+  console.log(paper.project.activeLayer.children);
   console.groupEnd();
 };
 
@@ -76,13 +81,20 @@ export const onBrushMouseDrag = (event: paper.MouseEvent) => {
 
   // selection이 있을때만
   if (selection) {
-    const c1 = new paper.Path.Circle({
-      center: event.point,
-      radius,
-    });
+    const c1 = new paper.CompoundPath(
+      new paper.Path.Circle({
+        center: event.point,
+        radius,
+      }),
+    );
 
     // 새로운 brush를 unite
-    const newSelection = selection.unite(c1);
+    const newSelection = new paper.CompoundPath(selection.unite(c1));
+    // const newSelection = selection.unite(c1);
+    newSelection.fillColor = fillColor;
+    newSelection.strokeColor = strokeColor;
+    newSelection.strokeWidth = strokeWidth;
+
     c1.remove();
     selection.remove();
     selection = newSelection;
