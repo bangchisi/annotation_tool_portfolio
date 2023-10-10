c9c705f3cf34b68d96a704d2faf0f3df92dfbc07
bangchisi <ahdtpf13@naver.com>
2023-10-10 17:01:47 +0900

    api request url to ip:port/endpoint format

--- src/routes/Annotator/models/Annotator.model.ts
+++ src/routes/Annotator/models/Annotator.model.ts
@@ -1,26 +1,34 @@
 import axios from 'axios';
 
-const baseURL = '/api/annotator/';
+const baseURL = 'api/annotator';
 
 const initialCategories = {
   data: [
     {
       id: 0,
       name: 'human',
+      annotations: [],
     },
     {
       id: 1,
       name: 'animal',
+      annotations: [],
     },
     {
       id: 2,
       name: 'building',
+      annotations: [],
     },
     {
       id: 3,
       name: 'machine',
+      annotations: [],
     },
   ],
+};
+
+const initialPaths = {
+  data: [],
 };
 
 const initialAnnotations = {
@@ -29,39 +37,59 @@ const initialAnnotations = {
   },
 };
 
+const getServerAndBaseURL = (): string => {
+  return `${process.env.REACT_APP_IP}:${process.env.REACT_APP_PORT}/${baseURL}`;
+};
+
+const axiosErrorHandler = (error: unknown, message: string) => {
+  if (error instanceof Error) {
+    console.log(message);
+    console.dir(error.stack);
+  }
+
+  return null;
+};
+
 const AnnotatorModel = {
+  // GET, categories 불러옴
   getCategories: async (datasetId: number, imageId: number) => {
     try {
+      const url = `${getServerAndBaseURL()}/categories/${datasetId}/${imageId}`;
       const response =
         process.env.NODE_ENV === 'development'
           ? initialCategories
-          : await axios.get(`${baseURL}/categories/${datasetId}/${imageId}`);
+          : await axios.get(url);
 
       return response.data;
     } catch (error) {
-      console.log('Failed to get categories');
-      if (error instanceof Error) {
-        console.log(error.stack);
-      }
+      axiosErrorHandler(error, 'Failed to get categories');
+    }
+  },
+  // GET, paths 불러옴
+  getPaths: async () => {
+    try {
+      const url = `${getServerAndBaseURL()}/paths`;
+      const response =
+        process.env.NODE_ENV === 'development'
+          ? initialPaths
+          : await axios.get(url);
 
-      return null;
+      return response.data;
+    } catch (error) {
+      axiosErrorHandler(error, 'Failed to get paths');
     }
   },
   getAnnotations: async (datasetId: number, imageId: number) => {
     try {
+      const url = `${getServerAndBaseURL()}/annotations/${datasetId}/${imageId}`;
       const response =
         process.env.NODE_ENV === 'development'
           ? initialAnnotations
-          : await axios.get(`${baseURL}/annotations/${datasetId}/${imageId}`);
+          : await axios.get(url);
 
       return response.data;
     } catch (error) {
-      if (error instanceof Error) {
-        console.log('Failed to get annotations');
-        console.log(error.stack);
-
-        return null;
-      }
+      axiosErrorHandler(error, 'Failed to get annotations');
     }
   },
 };
