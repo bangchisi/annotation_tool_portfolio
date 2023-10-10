6476731ed06dbd893c01b83b6b300dba4bb863ca
bangchisi <ahdtpf13@naver.com>
2023-10-10 17:03:41 +0900

    remove unnecessary code in Annotator.tsx

--- src/routes/Annotator/Annotator.tsx
+++ src/routes/Annotator/Annotator.tsx
@@ -3,13 +3,10 @@ import LeftSidebar from './components/LeftSidebar/LeftSidebar';
 import { Container } from './Annotator.style';
 import Workbench from './components/Workbench/Workbench';
 import { useAppDispatch, useAppSelector } from 'App.hooks';
-import {
-  setCategories,
-  setCurrentCategory,
-  updateCurrentCategory,
-} from './slices/annotatorSlice';
+import { setCategories, setCurrentCategory } from './slices/annotatorSlice';
 import AnnotatorModel from './models/Annotator.model';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
+import PathStore from './utils/PathStore';
 
 export enum Tool {
   Select,
@@ -19,6 +16,9 @@ export enum Tool {
   SAM,
 }
 
+// path 정보들을 저장한 배열
+export let paths: PathStore;
+
 export default function Annotator() {
   const dispatch = useAppDispatch();
   const selectedTool = useAppSelector((state) => state.annotator.selectedTool);
@@ -26,19 +26,8 @@ export default function Annotator() {
   const currentCategory = useAppSelector(
     (state) => state.annotator.currentCategory,
   );
-  const currentAnnotation = useAppSelector(
-    (state) => state.annotator.currentAnnotation,
-  );
-  // undo 기능을 위해 prevAnnotation을 store에 undo list를 만들어 저장하는게 좋을듯
-  const [prevAnnotation, setPrevAnnotation] = useState(currentAnnotation);
-
-  // 처음 한번 categories 세팅
+  // init categories, paths
   useEffect(() => {
-    // console.group(
-    //   '%cAnnotator.tsx, useEffect, [categories, dispatch]',
-    //   'color: green',
-    // );
-
     // categories 가져오는 async 함수
     async function fetchCategories() {
       try {
@@ -56,76 +45,27 @@ export default function Annotator() {
       }
     }
 
+    async function fetchPaths() {
+      const pathArray = await AnnotatorModel.getPaths();
+      paths = new PathStore(pathArray);
+    }
+
     if (categories.length === 0) {
       // categories가 비어있을 때만 데이터를 가져옴
       fetchCategories();
+      fetchPaths();
     }
-    // console.groupEnd();
   }, [dispatch]);
-
-  // currentCategory 세팅 <- categories init에서 이미 하기 때문에 필요 없는 함수. 지울 예정
-  // useEffect(() => {
-  //   // console.group(
-  //   //   '%cAnnotator.tsx, useEffect, [currentCategory]',
-  //   //   'color: green',
-  //   // );
-  //   if (categories.length > 0) {
-  //     console.dir('caetgories');
-  //     console.dir(categories);
-  //     dispatch(setCurrentCategory(categories[0]));
-  //   }
-  //   console.groupEnd();
-  // }, [categories, dispatch]);
 
   // currentCategory 변경 -> categories 갱신
   useEffect(() => {
-    // console.group(
-    //   '%cAnnotator.tsx, useEffect, [currentCategory]',
-    //   'color: green',
-    // );
     if (currentCategory) {
       const updatedCategories = categories.map((category) =>
         category.id === currentCategory.id ? currentCategory : category,
       );
       dispatch(setCategories(updatedCategories));
     }
-    // console.groupEnd();
   }, [currentCategory, dispatch]);
-
-  // currentAnnotation.path 변경 -> currentCategory 갱신
-  useEffect(() => {
-    // console.group(
-    //   '%cAnnotator.tsx, useEffect, [currentAnnotation, currentAnnotation?.path]',
-    //   'color: green',
-    // );
-
-    // console.log('prevAnnotation', prevAnnotation);
-    // console.log('currentAnnotation', currentAnnotation);
-
-    // currentAnnotation과 이전 currentAnnotation의 id 비교
-    if (prevAnnotation && prevAnnotation.id === currentAnnotation?.id) {
-      // id가 같으면 path만 변경
-      console.log('Only path has changed.');
-      // currentAnnotation의 path 변경 로직 작성
-      // currentCategory.annotations 업데이트 로직 작성
-      // console.log(currentAnnotation);
-      dispatch(updateCurrentCategory(currentAnnotation));
-    } else {
-      // id가 다르면 currentAnnotation 전체를 변경
-      console.log('Annotation has changed.');
-      // TODO: currentAnnotation 전체를 업데이트하는 로직 작성
-      // selection에 currentAnnotation.path를 넣어야 함
-
-      // box tool일때만 categories까지 업데이트
-      if (selectedTool === Tool.Box) {
-        dispatch(updateCurrentCategory(currentAnnotation));
-      }
-    }
-
-    // 현재 currentAnnotation을 이전 currentAnnotation으로 설정
-    setPrevAnnotation(currentAnnotation);
-    // console.groupEnd();
-  }, [currentAnnotation, dispatch]);
 
   return (
     <Container>
