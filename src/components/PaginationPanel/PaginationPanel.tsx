bc677120933daf6ae7f6cedbc0ccc1f332d8f1e9
bangchisi <ahdtpf13@naver.com>
2023-10-22 22:32:19 +0900

    pagination panel component

---
+++ src/components/PaginationPanel/PaginationPanel.tsx
@@ -0,0 +1,26 @@
+import { Pagination, PaginationItem } from '@mui/material';
+import { Container } from './PaginationPanel.style';
+
+interface PaginationProps {
+  onCurrentPageChange: (
+    event: React.ChangeEvent<unknown>,
+    page: number,
+  ) => void;
+  currentPage: number;
+  lastPage: number;
+}
+
+export default function PaginationPanel(props: PaginationProps) {
+  const { onCurrentPageChange, currentPage, lastPage } = props;
+  return (
+    <Container>
+      <Pagination
+        onChange={onCurrentPageChange}
+        defaultPage={1}
+        count={Math.ceil(lastPage / 10)}
+        page={currentPage}
+        renderItem={(item) => <PaginationItem {...item} />}
+      />
+    </Container>
+  );
+}
