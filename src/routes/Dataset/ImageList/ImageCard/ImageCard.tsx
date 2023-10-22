e51f92a135a1098d9216c5cc3765e40a1c4d1a93
bangchisi <ahdtpf13@naver.com>
2023-10-22 22:34:11 +0900

    image card component

--- src/routes/Dataset/ImageList/ImageCard/ImageCard.tsx
+++ src/routes/Dataset/ImageList/ImageCard/ImageCard.tsx
@@ -1,5 +1,22 @@
+import { getImagePath } from 'helpers/ImagesHelpers';
 import { Container } from './ImageCard.style';
+import { Link } from 'react-router-dom';
 
-export default function ImageCard() {
-  return <Container></Container>;
+interface ImageCardProps {
+  imageId: number;
+}
+
+export default function ImageCard(props: ImageCardProps) {
+  const { imageId } = props;
+
+  const imagePath = getImagePath(Number(imageId), 200);
+  const link = `/annotator/${imageId}`;
+  return (
+    <Container>
+      <Link to={link}>
+        <img src={imagePath} />
+        <div>이미지 이름 (id: {imageId})</div>
+      </Link>
+    </Container>
+  );
 }
