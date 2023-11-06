03c28fd444690e70c637e5967215d87f0369ebed
ghlee <ghlee@tomocube.com>
2023-11-06 15:03:06 +0900

    load spinner background style

--- src/components/LoadingSpinner/LoadingSpinner.style.ts
+++ src/components/LoadingSpinner/LoadingSpinner.style.ts
@@ -9,6 +9,7 @@ export const Container = styled('div')(() => {
     height: '100%',
     // backgroundColor: 'rgba(255, 255, 255, 1.0)',
     backgroundColor: 'rgba(0, 0, 0, 0.3)',
+    backdropFilter: 'blur(5px)',
   };
 });
 
