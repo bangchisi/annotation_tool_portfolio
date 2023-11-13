86edc80fef10efbcb0ce28e35ce0ea72d2ff6e76
ghlee <ghlee@tomocube.com>
2023-11-13 16:48:45 +0900

    add fine tuning api file

---
+++ src/models/Finetune.model.ts
@@ -0,0 +1,37 @@
+import axios from 'axios';
+
+const DEV_URL = `http://${process.env.REACT_APP_DEV_IP}:${process.env.REACT_APP_DEV_PORT}`;
+const SERVER_URL = `http://${process.env.REACT_APP_IP}:${process.env.REACT_APP_PORT}`;
+
+const FinetuneModel = {
+  checkAvailableDevice: () => {
+    const url =
+      process.env.NODE_ENV === 'development'
+        ? `${DEV_URL}/finetune/is_available`
+        : `${SERVER_URL}/finetune/is_available`;
+
+    return axios.get(url);
+  },
+  start: (datasetId: number, deviceId: number, modelType: string) => {
+    const url =
+      process.env.NODE_ENV === 'development'
+        ? `${DEV_URL}/finetune`
+        : `${SERVER_URL}/finetune`;
+
+    return axios.post(url, {
+      dataset_id: datasetId,
+      device_id: deviceId,
+      vit_model_type: modelType,
+    });
+  },
+  getAnnotatedImagesCount: (datasetId: number) => {
+    const url =
+      process.env.NODE_ENV === 'development'
+        ? `${DEV_URL}/finetune/annotated/${datasetId}`
+        : `${SERVER_URL}/finetune/annotated/${datasetId}`;
+
+    return axios.get(url);
+  },
+};
+
+export default FinetuneModel;
