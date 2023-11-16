e398418ef410c14163531c0ca01a4956a6d455ae
ghlee <ghlee@tomocube.com>
2023-11-16 14:08:51 +0900

    TEMP disabled minimap

--- src/routes/Annotator/components/RightSidebar/RightSidebar.tsx
+++ src/routes/Annotator/components/RightSidebar/RightSidebar.tsx
@@ -1,14 +1,14 @@
 import { Container } from './RightSidebar.style';
 import Controls from './Controls/Controls';
 import Preferences from './Preferences/Preferences';
-import Minimap from './Minimap/Minimap';
+// import Minimap from './Minimap/Minimap';
 
 export default function RightSidebar() {
   return (
     <Container>
       <Controls />
       <Preferences />
-      <Minimap view={null} image={null} />
+      {/* <Minimap view={null} image={null} /> */}
     </Container>
   );
 }
