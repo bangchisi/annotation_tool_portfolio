36e5d8e41f1358b7eab6db3fd540737c7592764f
ghlee <ghlee@tomocube.com>
2023-11-16 14:10:41 +0900

    add preference in store

--- src/routes/Auth/slices/authSlice.ts
+++ src/routes/Auth/slices/authSlice.ts
@@ -6,11 +6,19 @@ interface UserType {
   isOnline: boolean;
 }
 
+export interface PreferenceType {
+  [key: string]: unknown;
+  brushRadius: number;
+}
+
 const initialState = {
   user: {
     userId: '',
     userName: '',
     isOnline: false,
+  },
+  preference: {
+    brushRadius: 10,
   },
   isAuthenticated: false,
 };
@@ -25,9 +33,16 @@ const authSlice = createSlice({
     setIsAuthenticated: (state, action: PayloadAction<boolean>) => {
       state.isAuthenticated = action.payload;
     },
+    setPreference: (state, action: PayloadAction<PreferenceType>) => {
+      state.preference = action.payload;
+    },
+    setBrushRadius: (state, action: PayloadAction<number>) => {
+      state.preference.brushRadius = action.payload;
+    },
   },
 });
 
-export const { setUser, setIsAuthenticated } = authSlice.actions;
+export const { setUser, setIsAuthenticated, setPreference, setBrushRadius } =
+  authSlice.actions;
 
 export default authSlice.reducer;
