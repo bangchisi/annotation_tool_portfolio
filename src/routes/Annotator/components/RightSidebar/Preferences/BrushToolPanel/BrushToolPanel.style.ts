c1342e4084028eee07adf497a502224c9d75f924
ghlee <ghlee@tomocube.com>
2023-11-16 14:06:30 +0900

    [WIP] Brush Tool preference panel

--- src/routes/Annotator/components/RightSidebar/Preferences/BrushToolPanel/BrushToolPanel.style.ts
+++ src/routes/Annotator/components/RightSidebar/Preferences/BrushToolPanel/BrushToolPanel.style.ts
@@ -1,8 +1,24 @@
-import { styled } from '@mui/material';
+import { Box, TextField, Typography, styled } from '@mui/material';
 
 export const Container = styled('div')(() => {
   return {
     width: '100%',
     height: '100%',
   };
+});
+
+export const ToolName = styled(Typography)(() => {
+  return {};
+});
+
+export const PrefName = styled(Typography)(() => {
+  return {};
+});
+
+export const BrushRadiusContainer = styled(Box)(() => {
+  return {};
+});
+
+export const BrushRadius = styled(TextField)(() => {
+  return {};
 });
--- src/routes/Annotator/components/RightSidebar/Preferences/BrushToolPanel/BrushToolPanel.tsx
+++ src/routes/Annotator/components/RightSidebar/Preferences/BrushToolPanel/BrushToolPanel.tsx
@@ -1,9 +1,38 @@
-import { Container } from './BrushToolPanel.style';
+import { useAppDispatch, useAppSelector } from 'App.hooks';
+import {
+  BrushRadius,
+  BrushRadiusContainer,
+  Container,
+  PrefName,
+  ToolName,
+} from './BrushToolPanel.style';
+import { setBrushRadius } from 'routes/Auth/slices/authSlice';
 
 export default function BrushToolPanel() {
+  const dispatch = useAppDispatch();
+  const brushRadius = useAppSelector(
+    (state) => state.auth.preference.brushRadius,
+  );
+
+  function onBrushRadiusChange(
+    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+  ) {
+    dispatch(setBrushRadius(Number(event.target.value)));
+  }
+
   return (
     <Container>
-      <div>Brush panel</div>
+      <ToolName variant="subtitle2">Brush panel</ToolName>
+      <BrushRadiusContainer>
+        <PrefName variant="h6">radius</PrefName>
+        <BrushRadius
+          type="number"
+          value={brushRadius}
+          onChange={(event) => {
+            onBrushRadiusChange(event);
+          }}
+        />
+      </BrushRadiusContainer>
     </Container>
   );
 }
