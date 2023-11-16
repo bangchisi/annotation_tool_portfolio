30e68f577340b605e8e7bb81d1d072f3bbd4a433
ghlee <ghlee@tomocube.com>
2023-11-16 14:06:06 +0900

    [WIP] SAM Tool preference panel

--- src/routes/Annotator/components/RightSidebar/Preferences/SAMToolPanel/SAMToolPanel.style.ts
+++ src/routes/Annotator/components/RightSidebar/Preferences/SAMToolPanel/SAMToolPanel.style.ts
@@ -1,1 +1,41 @@
-import { styled } from '@mui/material';
+import { Box, Button, styled } from '@mui/material';
+
+export const Container = styled(Box)(() => {
+  return {};
+});
+
+export const ModelContainer = styled(Box)(() => {
+  return {
+    display: 'flex',
+    justifyContent: 'space-between',
+    marginBottom: 7,
+  };
+});
+
+export const SelectModel = styled(Box)(() => {
+  return {
+    display: 'flex',
+    flexDirection: 'column',
+    flex: 1,
+  };
+});
+
+export const SliderContainer = styled(Box)(() => {
+  return {
+    display: 'flex',
+    flexDirection: 'column',
+  };
+});
+
+export const SliderContent = styled(Box)(() => {
+  return {
+    display: 'flex',
+    flexDirection: 'column',
+  };
+});
+
+export const EverythingButton = styled(Button)(() => {
+  return {
+    width: '100%',
+  };
+});
--- src/routes/Annotator/components/RightSidebar/Preferences/SAMToolPanel/SAMToolPanel.tsx
+++ src/routes/Annotator/components/RightSidebar/Preferences/SAMToolPanel/SAMToolPanel.tsx
@@ -1,26 +1,98 @@
-import { Fragment } from 'react';
-import { Box, Button, Slider, Typography } from '@mui/material';
+import {
+  MenuItem,
+  Select,
+  SelectChangeEvent,
+  Slider,
+  Typography,
+} from '@mui/material';
+import {
+  Container,
+  EverythingButton,
+  ModelContainer,
+  SelectModel,
+  SliderContainer,
+  SliderContent,
+} from './SAMToolPanel.style';
+import { useAppDispatch, useAppSelector } from 'App.hooks';
+import { setSAMModelLoading } from 'routes/Annotator/slices/annotatorSlice';
+import {
+  dummy,
+  loadSAM,
+} from 'routes/Annotator/components/Workbench/Canvas/tools/SAMTool';
 
 export default function SAMToolPanel() {
+  const dispatch = useAppDispatch();
+  const preference = useAppSelector((state) => state.auth.preference);
+
+  function onChangeModel(event: SelectChangeEvent<string>) {
+    // ...
+    loadSAM(event.target.value);
+  }
+
   return (
-    <Fragment>
-      <Button variant="outlined">Everything</Button>
-      <Button variant="outlined">Everything</Button>
-      <Button variant="outlined">Everything</Button>
-      <Button variant="outlined">Everything</Button>
-      <Button variant="outlined">Everything</Button>
-      <Button variant="outlined">Everything</Button>
-      <Box sx={{ padding: 1 }}>
-        <Typography gutterBottom>Points</Typography>
-        <Slider
-          defaultValue={20}
-          valueLabelDisplay="auto"
-          step={5}
-          marks
-          min={10}
-          max={100}
-        />
-      </Box>
-    </Fragment>
+    <Container>
+      <Typography variant="subtitle2">SAM</Typography>
+      <ModelContainer>
+        <SelectModel>
+          <Typography variant="caption">기존 모델</Typography>
+          <Select
+            onChange={(event) => onChangeModel(event)}
+            size="small"
+            defaultValue="vit_h"
+          >
+            <MenuItem value="vit_h">vit_h</MenuItem>
+            <MenuItem value="vit_l">vit_l</MenuItem>
+            <MenuItem value="vit_b">vit_b</MenuItem>
+          </Select>
+        </SelectModel>
+        <SelectModel>
+          <Typography variant="caption">Finetuned 모델</Typography>
+          <Select size="small" defaultValue="vit_h">
+            <MenuItem value="vit_h">vit_h</MenuItem>
+            <MenuItem value="vit_l">vit_l</MenuItem>
+            <MenuItem value="vit_b">vit_b</MenuItem>
+          </Select>
+        </SelectModel>
+      </ModelContainer>
+      <Typography variant="subtitle2">EVERYTHING</Typography>
+      <SliderContainer>
+        <SliderContent>
+          <Typography variant="caption">pred_iou_thresh</Typography>
+          <Slider
+            defaultValue={0.88}
+            valueLabelDisplay="auto"
+            step={0.01}
+            marks
+            min={0.0}
+            max={1.0}
+          />
+        </SliderContent>
+        <SliderContent>
+          <Typography variant="caption">box_nms_thresh</Typography>
+          <Slider
+            defaultValue={0.7}
+            valueLabelDisplay="auto"
+            step={0.1}
+            marks
+            min={0.0}
+            max={1.0}
+          />
+        </SliderContent>
+        <SliderContent>
+          <Typography variant="caption">points_per_side</Typography>
+          <Slider
+            defaultValue={16}
+            valueLabelDisplay="auto"
+            step={4}
+            marks
+            min={12}
+            max={40}
+          />
+        </SliderContent>
+      </SliderContainer>
+      <EverythingButton onClick={dummy} variant="contained" color="primary">
+        START EVERYTHING
+      </EverythingButton>
+    </Container>
   );
 }
