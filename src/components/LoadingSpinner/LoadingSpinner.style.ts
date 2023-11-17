95a503483bf7c8ddbf62c062a481887f44e53ab1
bangchisi <ahdtpf13@naver.com>
2023-11-17 19:55:45 +0900

    loading spinner position absolute -> fixed

--- src/components/LoadingSpinner/LoadingSpinner.style.ts
+++ src/components/LoadingSpinner/LoadingSpinner.style.ts
@@ -2,12 +2,11 @@ import { CircularProgress, Typography, styled } from '@mui/material';
 
 export const Container = styled('div')(() => {
   return {
-    position: 'absolute',
+    position: 'fixed',
     top: 0,
     left: 0,
     width: '100%',
     height: '100%',
-    // backgroundColor: 'rgba(255, 255, 255, 1.0)',
     backgroundColor: 'rgba(0, 0, 0, 0.3)',
     backdropFilter: 'blur(5px)',
   };
