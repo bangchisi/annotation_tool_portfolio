3d0dd7becf3cee5a5f6f3775516bd8c7da36b563
bangchisi <ahdtpf13@naver.com>
2023-11-18 13:16:01 +0900

    ADD check dataset is on train helper

---
+++ src/routes/Dataset/helpers/DatasetHelpers.ts
@@ -0,0 +1,20 @@
+import { axiosErrorHandler } from 'helpers/Axioshelpers';
+import FinetuneModel from 'models/Finetune.model';
+import { LogType } from 'routes/Models/Models';
+
+export async function getIsOnTrain(userId: string, datasetId: number) {
+  try {
+    const response = await FinetuneModel.getLogs(userId);
+    const logs = response.data as LogType[];
+
+    const log = logs.find((log) => log.datasetId === datasetId);
+
+    if (!log) return false;
+    if (log.isDone) return false;
+
+    return true;
+  } catch (error) {
+    axiosErrorHandler(error, 'Failed to get onTrain flag');
+    return false;
+  }
+}
