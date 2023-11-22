5b3869cebf77b1ad5ac127d749c3444d59ea8a9e
bangchisi <ahdtpf13@naver.com>
2023-11-22 23:39:27 +0900

    [WIP] eraser tool panel

--- src/routes/Annotator/components/RightSidebar/Preferences/EraserToolPanel/EraserToolPanel.tsx
+++ src/routes/Annotator/components/RightSidebar/Preferences/EraserToolPanel/EraserToolPanel.tsx
@@ -1,9 +1,36 @@
-import { Container } from './EraserToolPanel.style';
+import { useAppDispatch, useAppSelector } from 'App.hooks';
+import {
+  Container,
+  EraserRadius,
+  EraserRadiusContainer,
+  PrefName,
+  ToolName,
+} from './EraserToolPanel.style';
+import { selectAuth, setEraserRadius } from 'routes/Auth/slices/authSlice';
 
 export default function EraserToolPanel() {
+  const dispatch = useAppDispatch();
+  const { eraserRadius } = useAppSelector(selectAuth).preference;
+
+  function onEraserRadiusChange(
+    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+  ) {
+    dispatch(setEraserRadius(Number(event.target.value)));
+  }
+
   return (
     <Container>
-      <div>Eraser panel</div>
+      <ToolName variant="subtitle2">Brush panel</ToolName>
+      <EraserRadiusContainer>
+        <PrefName variant="h6">radius</PrefName>
+        <EraserRadius
+          type="number"
+          value={eraserRadius}
+          onChange={(event) => {
+            onEraserRadiusChange(event);
+          }}
+        />
+      </EraserRadiusContainer>
     </Container>
   );
 }
