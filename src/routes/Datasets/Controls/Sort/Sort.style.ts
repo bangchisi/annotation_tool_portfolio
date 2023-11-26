072d352f3f22192f05fab9695e7e1c8b6c9620c2
bangchisi <ahdtpf13@naver.com>
2023-11-26 17:22:51 +0900

    CHANGE sort button style

--- src/routes/Datasets/Controls/Sort/Sort.style.ts
+++ src/routes/Datasets/Controls/Sort/Sort.style.ts
@@ -1,5 +1,7 @@
 import { styled } from '@mui/material';
 
 export const Container = styled('div')(() => {
-  return {};
+  return {
+    marginLeft: '60px',
+  };
 });
--- src/routes/Datasets/Controls/Sort/Sort.tsx
+++ src/routes/Datasets/Controls/Sort/Sort.tsx
@@ -4,7 +4,16 @@ import { Container } from './Sort.style';
 export default function Sort() {
   return (
     <Container>
-      <Button variant="contained" size="small" color="success">
+      <Button
+        variant="contained"
+        size="small"
+        color="success"
+        sx={{
+          width: '85px',
+          height: '40px',
+          backgroundColor: '#1F883D',
+        }}
+      >
         Sort by
       </Button>
     </Container>
