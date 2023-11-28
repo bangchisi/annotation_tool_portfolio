6158a7d7110bf06d827f83362a51e5c6a2614509
bangchisi <ahdtpf13@naver.com>
2023-11-29 02:18:06 +0900

    CHORE error handler

--- src/routes/Annotator/components/LeftSidebar/LeftSidebar.tsx
+++ src/routes/Annotator/components/LeftSidebar/LeftSidebar.tsx
@@ -1,6 +1,7 @@
+import { useCallback } from 'react';
+import paper from 'paper';
 import { Box, List, Divider } from '@mui/material';
 import ToolIcon from './ToolIcon';
-import paper from 'paper';
 
 import BackHandOutlinedIcon from '@mui/icons-material/BackHandOutlined';
 import RectangleOutlinedIcon from '@mui/icons-material/RectangleOutlined';
@@ -26,12 +27,6 @@ export default function LeftSidebar() {
     const children = paper.project.activeLayer.children;
 
     const categoriesToUpdate = JSON.parse(JSON.stringify(categories));
-
-    // @이슈: 브러쉬 커브 없던 거
-    // 이유: 기존 숨겨진 속성이 없는 채로 save를 했음.
-    // segmentation만 바로 보내니까 브러쉬 커브가 없던 거였음.
-    // 전체 객체를 serialize해서 보내는 것이 아니라 일부(segmentation)만 보냈음
-    // 해결방법: 전체 객체를 serialize해서 보내기. 그리고 data 객체도 서버에 저장하고 load하면 보내기
     children.forEach((child) => {
       if (child instanceof paper.CompoundPath) {
         const { categoryId, annotationId } = child.data;
@@ -62,6 +57,9 @@ export default function LeftSidebar() {
         imageId,
         categoriesToUpdate,
       );
+      if (response.status === 200) {
+        console.log('Successfully saved annotator data');
+      }
     } catch (error) {
       axiosErrorHandler(error, 'Failed to save annotator data');
     } finally {
