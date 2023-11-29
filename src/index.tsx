734d9aa23418dbe3764945941be14b63f4c1d361
ghlee <ghlee@tomocube.com>
2023-11-29 14:36:40 +0900

    CHORE removed comment

--- src/index.tsx
+++ src/index.tsx
@@ -26,41 +26,6 @@ import { Provider } from 'react-redux';
 import store from 'store';
 import Authenticated from 'components/Authenticated';
 
-/**
- * 서버 정보?, 로그인 정보
- * 사용할 페이지: 모든 페이지
- */
-
-/** datasets
- * dataset 목록
- * 사용할 페이지: datasets
- */
-
-/** dataset
- * datasetId, 이미지 목록
- * 사용할 페이지: dataset, annotator
- */
-
-/** 이미지
- * imageId, 이미지 자체 정보(width, height, url..), category 목록
- * 사용할 페이지: annotator
- * /
-
-/**
- * category
- * categoryId, 이름, annotation 목록
- * 사용할 페이지: annotator
- */
-
-/** annotation
- * annotationId, polygon 정보
- * 사용할 페이지: annotator
- */
-
-/**store에 저장할 정보
- * 서버정보와 로그인 정보는 모든 페이지에서 쓰니까 store에 저장
- */
-
 const router = createBrowserRouter([
   {
     path: '/',
