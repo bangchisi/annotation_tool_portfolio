3eb30d926aa3007baae7465c09f613e4a5999710
ghlee <ghlee@tomocube.com>
2023-12-04 21:25:14 +0900

    navbar style and logout button

--- src/components/Navigator/Navigator.style.ts
+++ src/components/Navigator/Navigator.style.ts
@@ -1,23 +1,55 @@
-import { AppBar, styled } from '@mui/material';
-import { Link } from 'react-router-dom';
+import { styled } from '@mui/material';
 
-interface LinkProps {
-  'data-selected': boolean;
-}
+export const Container = styled('header')`
+  display: flex;
+  justify-content: center;
+  border-bottom: 1px solid #20252c !important;
+  padding: 10px 32px 10px 16px !important;
+`;
 
-export const Container = styled(AppBar)(() => {
-  return {
-    display: 'flex',
-  };
-});
+export const Nav = styled('nav')`
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+`;
 
-export const LinkContainer = styled('div')(() => {
-  return {};
-});
+export const LogoContainer = styled('div')`
+  display: flex;
+  align-items: center;
+  h6 {
+    color: #0e1116;
+  }
+`;
 
-export const NavigatorLink = styled(Link)<LinkProps>((props) => {
-  const selected = props['data-selected'];
-  return {
-    fontWeight: selected ? 'bold' : 'normal',
-  };
-});
+export const NavMenuList = styled('ul')`
+  display: flex;
+  align-items: center;
+  flex-direction: row;
+  list-style: none;
+  margin: 0;
+`;
+
+export const NavMenuItem = styled('li')`
+  a {
+    font-size: 14px;
+    color: #0e1116;
+    padding: 6px;
+  }
+  a:hover {
+    border-radius: 6px;
+    background-color: #e7ecf0;
+    transition: background-color 0.2s ease-in-out;
+  }
+  a.active {
+    font-weight: 500;
+  }
+`;
+
+export const DropDownContainer = styled('div')`
+  margin-left: auto;
+
+  .user-info {
+    font-size: 14px;
+    color: #0e1116;
+  }
+`;
--- src/components/Navigator/Navigator.tsx
+++ src/components/Navigator/Navigator.tsx
@@ -1,15 +1,39 @@
-import { Typography } from '@mui/material';
+import BadgeOutlinedIcon from '@mui/icons-material/BadgeOutlined';
+import LogoutOutlinedIcon from '@mui/icons-material/LogoutOutlined';
+import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';
+import {
+  Button,
+  ClickAwayListener,
+  Divider,
+  Grow,
+  ListItemIcon,
+  ListItemText,
+  MenuItem,
+  MenuList,
+  Paper,
+  Popper,
+  Typography,
+} from '@mui/material';
+import { RouteMode } from 'App';
 import { useAppDispatch, useAppSelector } from 'App.hooks';
+import { useEffect, useRef, useState } from 'react';
 import { NavLink } from 'react-router-dom';
+import { clearSAM } from 'routes/Annotator/slices/SAMSlice';
+import { clearAnnotator } from 'routes/Annotator/slices/annotatorSlice';
 import AuthModel from 'routes/Auth/models/Auth.model';
 import {
   clearAuth,
   setIsAuthenticated,
   setUser,
 } from 'routes/Auth/slices/authSlice';
-import { RouteMode } from 'App';
-import { clearSAM } from 'routes/Annotator/slices/SAMSlice';
-import { clearAnnotator } from 'routes/Annotator/slices/annotatorSlice';
+import {
+  Container,
+  DropDownContainer,
+  LogoContainer,
+  Nav,
+  NavMenuItem,
+  NavMenuList,
+} from './Navigator.style';
 
 interface NavigatorProps {
   currentMode: RouteMode;
@@ -36,59 +60,134 @@ export default function Navigator(props: NavigatorProps) {
     dispatch(clearAnnotator());
     dispatch(clearSAM());
   };
+  const handleLogout = (userId: string) => {
+    console.log('logout');
+    onLogout(userId);
+    handleClose(new Event('click'));
+  };
+
+  const [open, setOpen] = useState(false);
+  const anchorRef = useRef<HTMLButtonElement>(null);
+
+  const handleToggle = () => {
+    setOpen((prevOpen) => !prevOpen);
+  };
+
+  const handleClose = (event: Event | React.SyntheticEvent) => {
+    if (
+      anchorRef.current &&
+      anchorRef.current.contains(event.target as HTMLElement)
+    ) {
+      return;
+    }
+
+    setOpen(false);
+  };
+
+  function handleListKeyDown(event: React.KeyboardEvent) {
+    if (event.key === 'Tab') {
+      event.preventDefault();
+      setOpen(false);
+    } else if (event.key === 'Escape') {
+      setOpen(false);
+    }
+  }
+
+  // return focus to the button when we transitioned from !open -> open
+  const prevOpen = useRef(open);
+  useEffect(() => {
+    if (prevOpen.current === true && open === false) {
+      anchorRef.current!.focus();
+    }
+
+    prevOpen.current = open;
+  }, [open]);
 
   return (
-    <nav id="nav" className="navbar">
-      <div className="nav-container">
-        <NavLink to="/datasets" className="nav-link">
-          <Typography sx={{ marginRight: '20px' }} variant="h6">
-            Annotator{process.env.NODE_ENV === 'development' && ' (dev)'}
-          </Typography>
-        </NavLink>
-        <div className="navbar-menu-container">
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/datasets">
-                Datasets
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="models">
-                Models
-              </NavLink>
-            </li>
-            <li className="nav-item"></li>
-          </ul>
-        </div>
-        <div className="nav-item dropstart">
-          <a
-            className="nav-link"
-            role="button"
-            data-bs-toggle="dropdown"
-            aria-expanded="false"
+    <Container>
+      <Nav>
+        <LogoContainer>
+          <NavLink to="/datasets" className="nav-link">
+            <Typography sx={{ marginRight: '20px' }} variant="h6">
+              Annotator{process.env.NODE_ENV === 'development' && ' (dev)'}
+            </Typography>
+          </NavLink>
+        </LogoContainer>
+        <NavMenuList>
+          <NavMenuItem>
+            <NavLink className="nav-link" to="/datasets">
+              Datasets
+            </NavLink>
+          </NavMenuItem>
+          <NavMenuItem>
+            <NavLink className="nav-link" to="models">
+              Models
+            </NavLink>
+          </NavMenuItem>
+        </NavMenuList>
+
+        <DropDownContainer>
+          <Button
+            ref={anchorRef}
+            id="composition-button"
+            aria-controls={open ? 'composition-menu' : undefined}
+            aria-expanded={open ? 'true' : undefined}
+            aria-haspopup="true"
+            onClick={handleToggle}
           >
             <Typography display="inline" variant="button" className="user-info">
               {user.userId}
             </Typography>
-          </a>
-          <ul className="dropdown-menu">
-            <li>
-              <a className="dropdown-item">Settings</a>
-            </li>
-            <li>
-              <hr className="dropdown-divider" />
-            </li>
-            <li>
-              <a
-                className="dropdown-item"
-                onClick={() => onLogout(user.userId)}
+          </Button>
+          <Popper
+            open={open}
+            anchorEl={anchorRef.current}
+            role={undefined}
+            placement="bottom-end"
+            transition
+          >
+            {({ TransitionProps, placement }) => (
+              <Grow
+                {...TransitionProps}
+                style={{
+                  transformOrigin: 'right top',
+                }}
               >
-                Logout
-              </a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
+                <Paper>
+                  <ClickAwayListener onClickAway={handleClose}>
+                    <MenuList
+                      autoFocusItem={open}
+                      id="composition-menu"
+                      aria-labelledby="composition-button"
+                      onKeyDown={handleListKeyDown}
+                    >
+                      <MenuItem onClick={handleClose}>
+                        <ListItemIcon>
+                          <BadgeOutlinedIcon fontSize="small" />
+                        </ListItemIcon>
+                        <ListItemText>Profile</ListItemText>
+                      </MenuItem>
+                      <MenuItem onClick={handleClose}>
+                        <ListItemIcon>
+                          <SettingsOutlinedIcon fontSize="small" />
+                        </ListItemIcon>
+                        <ListItemText>Settings</ListItemText>
+                      </MenuItem>
+                      <Divider />
+                      <MenuItem onClick={() => handleLogout(user.userId)}>
+                        <ListItemIcon>
+                          <LogoutOutlinedIcon fontSize="small" />
+                        </ListItemIcon>
+                        <ListItemText>Logout</ListItemText>
+                      </MenuItem>
+                    </MenuList>
+                  </ClickAwayListener>
+                </Paper>
+              </Grow>
+            )}
+          </Popper>
+        </DropDownContainer>
+      </Nav>
+    </Container>
   );
 }
