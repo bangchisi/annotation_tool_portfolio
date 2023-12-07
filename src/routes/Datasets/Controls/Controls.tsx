fc8870048023d82340d87c68f1f121590bdaa793
ghlee <ghlee@tomocube.com>
2023-12-07 19:42:59 +0900

    [WIP] search dataset

--- src/routes/Datasets/Controls/Controls.tsx
+++ src/routes/Datasets/Controls/Controls.tsx
@@ -2,18 +2,24 @@ import CreateDatasetModal from './CreateDatasetModal/CreateDatasetModal';
 import { Container, LeftControl } from './Controls.style';
 import SearchPanel from './SearchPanel/SearchPanel';
 import Sort from './Sort/Sort';
+import { DatasetType } from '../Datasets';
 
 interface ControlsProps {
+  datasets: DatasetType[];
+  setFilteredDatasets: React.Dispatch<React.SetStateAction<DatasetType[]>>;
   setDatasetList: (userId: string) => Promise<void>;
 }
 
 export default function Controls(props: ControlsProps) {
-  const { setDatasetList } = props;
+  const { datasets, setFilteredDatasets, setDatasetList } = props;
   return (
     <Container>
       <LeftControl>
         <Sort />
-        <SearchPanel />
+        <SearchPanel
+          datasets={datasets}
+          setFilteredDatasets={setFilteredDatasets}
+        />
       </LeftControl>
       <CreateDatasetModal setDatasetList={setDatasetList} />
     </Container>
--- src/routes/Datasets/Controls/SearchPanel/SearchPanel.tsx
+++ src/routes/Datasets/Controls/SearchPanel/SearchPanel.tsx
@@ -1,14 +1,48 @@
 import { TextField } from '@mui/material';
 import { FormContainer } from './SearchPanel.style';
+import { DatasetType } from 'routes/Datasets/Datasets';
+import { useRef, useState } from 'react';
 
-export default function SearchPanel() {
+interface SearchPanelProps {
+  datasets: DatasetType[];
+  setFilteredDatasets: React.Dispatch<React.SetStateAction<DatasetType[]>>;
+}
+
+export default function SearchPanel(props: SearchPanelProps) {
+  const { datasets, setFilteredDatasets } = props;
+  const [searchText, setSearchText] = useState('');
+  const searchRef = useRef<HTMLInputElement>(null);
+
   return (
     <FormContainer onSubmit={(e) => e.preventDefault()}>
       <TextField
+        ref={searchRef}
         type="text"
+        value={searchText}
         size="small"
         placeholder="Search.."
         className="dataset-search-input"
+        onChange={(e) => setSearchText(e.target.value)}
+        inputProps={{ ref: searchRef }}
+        onKeyUp={() => {
+          const filtered = datasets.filter(
+            (dataset) =>
+              dataset.superDatasetName
+                .toLocaleLowerCase()
+                .includes(searchText.toLocaleLowerCase()) ||
+              dataset.datasetName
+                .toLocaleLowerCase()
+                .includes(searchText.toLocaleLowerCase()) ||
+              dataset.categories.some((category) =>
+                category.name
+                  .toLocaleLowerCase()
+                  .includes(searchText.toLocaleLowerCase()),
+              ),
+          );
+
+          console.log(filtered);
+          setFilteredDatasets(filtered);
+        }}
       />
     </FormContainer>
   );
