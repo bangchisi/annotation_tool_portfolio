df6ef9b5ec5452267c1c3657ff712bc325bf9139
ghlee <ghlee@tomocube.com>
2023-12-19 14:24:59 +0900

    change description to description array

--- src/hooks/useOnboarding.tsx
+++ src/hooks/useOnboarding.tsx
@@ -4,23 +4,29 @@ import HelpOutlinedIcon from '@mui/icons-material/HelpOutlined';
 
 const ButtonContainer = styled(HelpOutlinedIcon)(() => {
   return {
-    position: 'absolute',
+    position: 'fixed',
     bottom: '5%',
     left: '2%',
     cursor: 'pointer',
     width: '3rem',
     height: '3rem',
+    zIndex: 1000,
     '&:hover': {
       color: '#3f51b5',
     },
   };
 });
 
-export const OnboardingContent = (title: string, description: string) => {
+export const OnboardingContent = (title: string, descriptions: string[]) => {
   return (
     <>
       <Typography variant="h5">{title}</Typography>
-      <Typography variant="body1">{description}</Typography>
+      {descriptions.length > 0 &&
+        descriptions.map((description) => (
+          <Typography key={description} variant="body1">
+            {description}
+          </Typography>
+        ))}
     </>
   );
 };
@@ -41,29 +47,25 @@ export const steps: { [key: string]: StepType[] } = {
   datasets: [
     {
       selector: '.search-step',
-      content: OnboardingContent(
-        'Dataset 검색',
+      content: OnboardingContent('Dataset 검색', [
         'Project, Dataset, 카테고리 이름으로 검색할 수 있습니다.',
-      ),
+      ]),
     },
     {
       selector: '.create-dataset-step',
-      content: OnboardingContent(
-        'Dataset 생성',
+      content: OnboardingContent('Dataset 생성', [
         '새로운 Dataset을 생성할 수 있습니다.',
-      ),
+      ]),
       action: clickElement,
     },
     {
       selector: '.modal-step',
-      content: OnboardingContent(
-        'Dataset 생성을 위한 항목',
-        `Super Dataset Name: 프로젝트 이름
-        Dataset Name: Dataset 이름
-        Description: Dataset에 대한 설명
-        Categories: Dataset에 포함될 카테고리
-        `,
-      ),
+      content: OnboardingContent('Dataset 생성을 위한 항목', [
+        'Super Dataset Name: 프로젝트 이름',
+        'Dataset Name: Dataset 이름',
+        'Description: Dataset에 대한 설명',
+        'Categories: Dataset에 포함될 카테고리',
+      ]),
       highlightedSelectors: ['.modal-step'],
       actionAfter: () => {
         const target = document.querySelector(
@@ -76,10 +78,9 @@ export const steps: { [key: string]: StepType[] } = {
     },
     {
       selector: '.dataset-list-step',
-      content: OnboardingContent(
-        'Dataset 선택',
-        'Dataset을 선택하면 해당 Dataset의 Annotation 페이지로 이동합니다.',
-      ),
+      content: OnboardingContent('Dataset 선택', [
+        'Dataset을 선택하면 해당 Dataset 상세 페이지로 이동합니다.',
+      ]),
       highlightedSelectors: ['.dataset-card'],
       action: clickElement,
       actionAfter: clickElement,
