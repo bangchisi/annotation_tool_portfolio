f0ebda856400be1ab35cdb835414b2161d2d6776
bangchisi <ahdtpf13@naver.com>
2023-12-26 15:41:16 +0900

    add useAxios using useSWR

---
+++ src/hooks/useAxios.tsx
@@ -0,0 +1,38 @@
+import axios from 'axios';
+import useSWR from 'swr';
+
+const API_URL = `http://${process.env.REACT_APP_IP}:${process.env.REACT_APP_PORT}`;
+
+export const useAxios = <T extends object>(
+  method:
+    | 'get'
+    | 'delete'
+    | 'post'
+    | 'put'
+    | 'GET'
+    | 'DELETE'
+    | 'POST'
+    | 'PUT' = 'get',
+  endpoint: string,
+  body?: T,
+) => {
+  const fetcher = async (url: string) => {
+    const response = await axios({
+      url: `${API_URL}${url}`,
+      method,
+      data: body,
+    });
+
+    return response.data;
+  };
+
+  const { data, error, isLoading } = useSWR(endpoint, fetcher);
+
+  return {
+    data,
+    isError: error,
+    isLoading,
+  };
+};
+
+export default useAxios;
