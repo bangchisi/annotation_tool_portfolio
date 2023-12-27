7e6d897f13b536bb6bd169444d7f4c895cb35626
bangchisi <ahdtpf13@naver.com>
2023-12-27 14:21:16 +0900

    change enhancedSWR hook get return type

--- src/hooks/useEnhancedSWR.tsx
+++ src/hooks/useEnhancedSWR.tsx
@@ -3,13 +3,13 @@ import useSWR from 'swr';
 
 const API_URL = `http://${process.env.REACT_APP_IP}:${process.env.REACT_APP_PORT}`;
 
-export const useAxios = <T extends object>(
-  method: 'get' | 'post' | 'GET' | 'POST' = 'get',
+export const useEnhancedSWR = <R,>(
+  method: 'get' | 'post' | 'GET' | 'POST',
   endpoint: string,
-  body?: T,
+  body?: object,
 ) => {
   const fetcher = async () => {
-    const response = await axios<T>({
+    const response = await axios<R>({
       url: `${API_URL}${endpoint}`,
       method,
       data: body,
@@ -18,9 +18,9 @@ export const useAxios = <T extends object>(
     return response.data;
   };
 
-  const { data, error, isLoading, mutate } = useSWR<T>(endpoint, fetcher);
+  const { data, error, isLoading, mutate } = useSWR<R>(endpoint, fetcher);
 
   return { data, isError: error, isLoading, mutate };
 };
 
-export default useAxios;
+export default useEnhancedSWR;
