b605f4fd173716e7ed9f0c7f6b535b2329353dca
bangchisi <ahdtpf13@naver.com>
2023-12-28 18:04:29 +0900

    remove saveData from annotator.model.ts

--- src/routes/Annotator/models/Annotator.model.ts
+++ src/routes/Annotator/models/Annotator.model.ts
@@ -1,23 +1,8 @@
 import axios from 'axios';
-import { CategoriesType } from '../Annotator.types';
 
 const SERVER_URL = `http://${process.env.REACT_APP_IP}:${process.env.REACT_APP_PORT}`;
 
 const AnnotatorModel = {
-  saveData: (
-    datasetId: number,
-    imageId: number,
-    categories: CategoriesType,
-  ) => {
-    const url = `${SERVER_URL}/annotator/data`;
-
-    return axios.post(url, {
-      image_id: imageId,
-      dataset_id: datasetId,
-      categories,
-      serialized_data: '',
-    });
-  },
   createAnnotation: (
     imageId: number,
     datasetId: number,
